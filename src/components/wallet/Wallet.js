f0a33cebc7e5c30b85c41cb2c851aa27e9363e83
Jason Kiraly <jaykiraly@gmail.com>
2018-11-30 16:47:52 -0800

    Cleaned up code.

--- src/components/wallet/Wallet.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {Jumbotron} from 'reactstrap'
-
-export default ({walletService}) => (
-	<Jumbotron>
-    <h1>ETH wallet: {walletService.publicKey}</h1>
-	</Jumbotron>
-)
